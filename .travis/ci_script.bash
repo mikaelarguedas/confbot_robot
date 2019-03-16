32fd2f1d58175aa46d9b90cc47cd6609dde7bd34
Mikael Arguedas <mikael.arguedas@gmail.com>
2019-03-16 15:09:12 +0100

    run CI against ros2 nightly
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>

--- .travis/ci_script.bash
+++ .travis/ci_script.bash
@@ -3,11 +3,12 @@ set -e
 
 cd $ROS2_WS
 
-# install dependencies
-apt-get -qq update && rosdep install -y \
-  --from-paths src \
-  --ignore-src \
-  --rosdistro $ROS_DISTRO
+# Installing crystal packages on nightly may lead to problem, commenting out for now
+# # install dependencies
+# rosdep update && apt-get -qq update && rosdep install -y \
+#   --from-paths src \
+#   --ignore-src \
+#   --rosdistro $ROS_DISTRO
 
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
