900c99c5e018bc88899c15d342480f64ce243cc8
Mikael Arguedas <mikael.arguedas@gmail.com>
2019-03-16 15:34:42 +0100

    keep rosdep install assuming only system depts to install if any
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>

--- .travis/ci_script.bash
+++ .travis/ci_script.bash
@@ -4,11 +4,11 @@ set -e
 cd $ROS2_WS
 
 # Installing crystal packages on nightly may lead to problem, commenting out for now
-# # install dependencies
-# rosdep update && apt-get -qq update && rosdep install -y \
-#   --from-paths src \
-#   --ignore-src \
-#   --rosdistro $ROS_DISTRO
+# install dependencies
+rosdep update && apt-get -qq update && rosdep install -y \
+  --from-paths src \
+  --ignore-src \
+  --rosdistro $ROS_DISTRO
 
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
